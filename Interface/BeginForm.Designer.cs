046185e9a893d0ce31afe4aaf17bddddd33d9985
MistyMt <fankai1999@foxmail.com>
2019-10-24 14:48:46 +0800

    图标居中

--- Interface/BeginForm.Designer.cs
+++ Interface/BeginForm.Designer.cs
@@ -36,18 +36,20 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Interface.Properties.Resources.mmexport1571898211320;
-            this.pictureBox1.Location = new System.Drawing.Point(524, 103);
+            this.pictureBox1.Location = new System.Drawing.Point(412, 82);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(189, 189);
+            this.pictureBox1.Size = new System.Drawing.Size(142, 151);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(577, 466);
+            this.button1.Location = new System.Drawing.Point(450, 373);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(89, 47);
+            this.button1.Size = new System.Drawing.Size(67, 38);
             this.button1.TabIndex = 1;
             this.button1.Text = "确定";
             this.button1.UseVisualStyleBackColor = true;
@@ -55,12 +57,13 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1290, 624);
+            this.ClientSize = new System.Drawing.Size(968, 499);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
