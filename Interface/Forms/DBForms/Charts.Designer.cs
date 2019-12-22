506ed591cb56545bf389a85d1588563cb71f5afd
MistyMt <fankai1999@foxmail.com>
2019-12-22 11:43:17 +0800

    完成多series绘图

--- Interface/Forms/DBForms/Charts.Designer.cs
+++ Interface/Forms/DBForms/Charts.Designer.cs
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea11 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend11 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series11 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
@@ -48,18 +48,20 @@
             this.button4 = new System.Windows.Forms.Button();
             this.label6 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button6 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // checkedListBox1
@@ -70,7 +72,7 @@
             this.checkedListBox1.Location = new System.Drawing.Point(5, 39);
             this.checkedListBox1.MultiColumn = true;
             this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(138, 84);
+            this.checkedListBox1.Size = new System.Drawing.Size(138, 372);
             this.checkedListBox1.TabIndex = 0;
             // 
             // comboBox1
@@ -113,6 +115,7 @@
             // 
             // splitContainer1.Panel2
             // 
+            this.splitContainer1.Panel2.Controls.Add(this.checkBox1);
             this.splitContainer1.Panel2.Controls.Add(this.button5);
             this.splitContainer1.Panel2.Controls.Add(this.button4);
             this.splitContainer1.Panel2.Cursor = System.Windows.Forms.Cursors.Default;
@@ -220,7 +223,7 @@
             this.button5.Location = new System.Drawing.Point(35, 48);
             this.button5.Margin = new System.Windows.Forms.Padding(2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(155, 28);
+            this.button5.Size = new System.Drawing.Size(166, 28);
             this.button5.TabIndex = 9;
             this.button5.Text = "保存图片";
             this.button5.UseVisualStyleBackColor = true;
@@ -231,7 +234,7 @@
             this.button4.Location = new System.Drawing.Point(35, 16);
             this.button4.Margin = new System.Windows.Forms.Padding(2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(155, 28);
+            this.button4.Size = new System.Drawing.Size(102, 28);
             this.button4.TabIndex = 0;
             this.button4.Text = "统计图";
             this.button4.UseVisualStyleBackColor = true;
@@ -261,6 +264,26 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "选择测点";
             // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(74, 432);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(69, 23);
+            this.button7.TabIndex = 14;
+            this.button7.Text = "清空";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(5, 432);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(69, 23);
+            this.button6.TabIndex = 14;
+            this.button6.Text = "选择";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(162, 102);
@@ -269,60 +292,71 @@
             this.textBox1.TabIndex = 13;
             this.textBox1.Text = "目前已选择测点：";
             // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(5, 129);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(69, 23);
-            this.button6.TabIndex = 14;
-            this.button6.Text = "选择";
-            this.button6.UseVisualStyleBackColor = true;
-            this.button6.Click += new System.EventHandler(this.button6_Click);
-            // 
-            // button7
-            // 
-            this.button7.Location = new System.Drawing.Point(74, 129);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(69, 23);
-            this.button7.TabIndex = 14;
-            this.button7.Text = "清空";
-            this.button7.UseVisualStyleBackColor = true;
-            this.button7.Click += new System.EventHandler(this.button7_Click);
-            // 
             // chart1
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(343, 129);
+            chartArea11.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea11);
+            legend11.Name = "Legend1";
+            this.chart1.Legends.Add(legend11);
+            this.chart1.Location = new System.Drawing.Point(363, 129);
             this.chart1.Margin = new System.Windows.Forms.Padding(2);
             this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(542, 338);
+            series11.ChartArea = "ChartArea1";
+            series11.Legend = "Legend1";
+            series11.Name = "Series1";
+            this.chart1.Series.Add(series11);
+            this.chart1.Size = new System.Drawing.Size(522, 338);
             this.chart1.TabIndex = 14;
             this.chart1.Text = "chart1";
             // 
-            // dataGridView1
+            // panel1
             // 
-            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(162, 129);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 23;
-            this.dataGridView1.Size = new System.Drawing.Size(176, 338);
-            this.dataGridView1.TabIndex = 15;
+            this.panel1.Location = new System.Drawing.Point(162, 130);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(176, 338);
+            this.panel1.TabIndex = 9;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(341, 130);
+            this.button8.Margin = new System.Windows.Forms.Padding(0);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(20, 20);
+            this.button8.TabIndex = 16;
+            this.button8.Text = "▲";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(341, 169);
+            this.button9.Margin = new System.Windows.Forms.Padding(0);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(20, 20);
+            this.button9.TabIndex = 16;
+            this.button9.Text = "▼";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(141, 22);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(60, 16);
+            this.checkBox1.TabIndex = 12;
+            this.checkBox1.Text = "显示值";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
             // Charts
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(885, 467);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button8);
             this.Controls.Add(this.chart1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.groupBox1);
@@ -334,12 +368,12 @@
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel1.PerformLayout();
             this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -368,6 +402,9 @@
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
--- Interface/Forms/DBForms/Charts.cs
+++ Interface/Forms/DBForms/Charts.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
 
+
 namespace Interface.Forms.DBForms
 {
     public partial class Charts : Form
@@ -139,72 +140,74 @@ namespace Interface.Forms.DBForms
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //清空datagridview1
-            if (dataGridView1.DataSource != null)
+            if (checkedListBox1.CheckedItems.Count == 1)
             {
 
-                System.Data.DataTable dt = (System.Data.DataTable)dataGridView1.DataSource;
 
-                dt.Rows.Clear();
+                // 清空panel1
+                panel1.Controls.Clear();
+                // 添加datagridview
+                DataGridView dataGridView1 = new DataGridView();
+                dataGridView1.Parent = this.panel1;
+                dataGridView1.Dock = DockStyle.Fill;
+                dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+                dataGridView1.RowHeadersVisible = false;
+                dataGridView1.BringToFront();
 
-                dataGridView1.DataSource = dt;
+                using (SQLiteConnection conn = new SQLiteConnection(config.DataSource))
+                {
+                    using (SQLiteCommand cmd = new SQLiteCommand())
+                    {
+                        conn.Open();
+                        cmd.Connection = conn;
 
+                        SQLiteHelper sh = new SQLiteHelper(cmd);
+
+
+                        try
+                        {
+                            DataTable dt = sh.Select("SELECT " + checkedListBox1.CheckedItems[0].ToString() + " FROM " + comboBox2.Text + " WHERE ID= " + comboBox3.Text + ";");
+                            dataGridView1.DataSource = dt;
+                        }
+                        catch (Exception ex)
+                        {
+                            DataTable dt = new DataTable();
+                            dt.Columns.Add("Error");
+                            dt.Rows.Add(ex.ToString());
+                            dataGridView1.DataSource = dt;
+                        }
+
+                        conn.Close();
+                    }
+                }
             }
-
             else
             {
-                dataGridView1.Rows.Clear();
-
-            }
-
-            using (SQLiteConnection conn = new SQLiteConnection(config.DataSource))
-            {
-                using (SQLiteCommand cmd = new SQLiteCommand())
-                {
-                    conn.Open();
-                    cmd.Connection = conn;
-
-                    SQLiteHelper sh = new SQLiteHelper(cmd);
-
-
-                    try
-                    {
-                        DataTable dt = sh.Select("SELECT " + checkedListBox1.CheckedItems[0].ToString() + " FROM " + comboBox2.Text + " WHERE ID= " + comboBox3.Text + ";");
-                        dataGridView1.DataSource = dt;
-                    }
-                    catch (Exception ex)
-                    {
-                        DataTable dt = new DataTable();
-                        dt.Columns.Add("Error");
-                        dt.Rows.Add(ex.ToString());
-                        dataGridView1.DataSource = dt;
-                    }
-
-                    conn.Close();
-                }
+                MessageBox.Show("请单选测点。");
             }
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            //清空datagridview1
-            if (dataGridView1.DataSource != null)
-            {
+            ////清空datagridview1
+            //if (dataGridView1.DataSource != null)
+            //{
 
-                System.Data.DataTable dt = (System.Data.DataTable)dataGridView1.DataSource;
+            //    System.Data.DataTable dt = (System.Data.DataTable)dataGridView1.DataSource;
 
-                dt.Rows.Clear();
+            //    dt.Rows.Clear();
 
-                dataGridView1.DataSource = dt;
+            //    dataGridView1.DataSource = dt;
 
-            }
+            //}
 
-            else
-            {
-                dataGridView1.Rows.Clear();
+            //else
+            //{
+            //    dataGridView1.Rows.Clear();
 
-            }
-
+            //}
+            panel1.Controls.Clear();
 
             using (SQLiteConnection conn = new SQLiteConnection(config.DataSource))
             {
@@ -215,20 +218,47 @@ namespace Interface.Forms.DBForms
 
                     SQLiteHelper sh = new SQLiteHelper(cmd);
 
-
-                    try
+                    //if (checkedListBox1.CheckedItems.Count == 1)
+                    //{
+                    //    // 单选测点时
+                    //    try
+                    //    {
+                    //        DataTable dt = sh.Select("SELECT " + checkedListBox1.CheckedItems[0].ToString() + ",检测时间" + " FROM " + comboBox2.Text + ";");
+                    //        dataGridView1.DataSource = dt;
+                    //    }
+                    //    catch (Exception ex)
+                    //    {
+                    //        DataTable dt = new DataTable();
+                    //        dt.Columns.Add("Error");
+                    //        dt.Rows.Add(ex.ToString());
+                    //        dataGridView1.DataSource = dt;
+                    //    }
+                    //}
+                    //else
+                    //{
+                    List<DataGridView> DGVs = new List<DataGridView>();
+                    for (int i = 0; i < checkedListBox1.CheckedItems.Count; i++)
                     {
-                        DataTable dt = sh.Select("SELECT " + checkedListBox1.CheckedItems[0].ToString() + ",检测时间" + " FROM " + comboBox2.Text + ";");
-                        dataGridView1.DataSource = dt;
-                    }
-                    catch (Exception ex)
-                    {
-                        DataTable dt = new DataTable();
-                        dt.Columns.Add("Error");
-                        dt.Rows.Add(ex.ToString());
-                        dataGridView1.DataSource = dt;
-                    }
+                        DataGridView dgv = new DataGridView();
+                        dgv.Parent = this.panel1;
+                        dgv.Dock = DockStyle.Fill;
+                        dgv.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                        dgv.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+                        dgv.RowHeadersVisible = false;
+                        dgv.BringToFront();
+                        try
+                        {
+                            DataTable dt = sh.Select("SELECT " + checkedListBox1.CheckedItems[i].ToString() + ",检测时间" + " FROM " + comboBox2.Text + ";");
+                            dgv.DataSource = dt;
+                        }
+                        catch (Exception)
+                        {
+                        }
 
+                    }
+                    Controls.AddRange(DGVs.ToArray());
+                    //}
+                    textBox1.Text = textBox1.Text;
                     conn.Close();
                 }
             }
@@ -236,9 +266,9 @@ namespace Interface.Forms.DBForms
 
         private void button4_Click(object sender, EventArgs e)
         {
+
             try
             {
-
                 this.chart1.Series.Clear();
                 //设置X/Y样式
                 chart1.ChartAreas[0].AxisY.Title = "测点温度";
@@ -254,49 +284,108 @@ namespace Interface.Forms.DBForms
                 //chart1.ChartAreas[0].AxisY.Enabled = AxisEnabled.False;
                 //chart1.Titles[0].Text = "";
 
-                //设置char样式
 
-                this.chart1.Series.Add("测点温度");
-
-                chart1.Series["测点温度"].MarkerColor = Color.Black;//设置标志
-                chart1.Series["测点温度"].MarkerSize = 3;
-                chart1.Series["测点温度"].MarkerStyle = MarkerStyle.Square;
-                chart1.Series["测点温度"].IsValueShownAsLabel = true;//是否显示值
-                chart1.Series["测点温度"].ChartType = SeriesChartType.Spline;//设置显示样式
-                chart1.Series["测点温度"].BorderWidth = 1;
-                chart1.Series["测点温度"].Color = Color.Blue;
-                chart1.Series["测点温度"].ToolTip = "测点" + checkedListBox1.CheckedItems[0].ToString() + comboBox2.Text + "时数据统计";
-                chart1.Series["测点温度"].YValueType = ChartValueType.Double;
-
-
-                List<string> time = new List<string>();
-                for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+                // 获取panel1中的dataGridViews组成的列表 s
+                List<DataGridView> s = new List<DataGridView>();
+                for (int i = 0; i < panel1.Controls.Count; i++)
                 {
-                    time.Add(dataGridView1.Rows[i].Cells["检测时间"].Value.ToString());
+                    s.Add((DataGridView)panel1.Controls[i]);
                 }
-
-                List<double> data = new List<double>();
-                for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+                List<double> allData = new List<double>();
+                // 由列表 s 生成 series。
+                for (int i = 0; i < s.Count; i++)
                 {
-                    data.Add(Convert.ToDouble(dataGridView1.Rows[i].Cells[checkedListBox1.CheckedItems[0].ToString()].Value));
-                }
+                    this.chart1.Series.Add(s[i].Columns[0].Name.ToString());
 
 
+                    //设置char样式
+                    chart1.Series[s[i].Columns[0].Name.ToString()].MarkerColor = Color.Black;//设置标志
+                    chart1.Series[s[i].Columns[0].Name.ToString()].MarkerSize = 3;
+                    chart1.Series[s[i].Columns[0].Name.ToString()].MarkerStyle = MarkerStyle.Square;
+                    chart1.Series[s[i].Columns[0].Name.ToString()].IsValueShownAsLabel = false;//是否显示值
+                    chart1.Series[s[i].Columns[0].Name.ToString()].ChartType = SeriesChartType.Spline;//设置显示样式
+                    chart1.Series[s[i].Columns[0].Name.ToString()].BorderWidth = 1;
+                    chart1.Series[s[i].Columns[0].Name.ToString()].Color = Color.FromArgb(i, i + 1, i + 2);
+                    //chart1.Series[s[i].Columns[0].Name.ToString()].ToolTip = "测点" + comboBox2.Text + "时数据统计";
+                    chart1.Series[s[i].Columns[0].Name.ToString()].YValueType = ChartValueType.Double;
 
-                int nowRow = 0;
-                if (dataGridView1.Rows.Count > 1)
-                {
-                    while (nowRow < dataGridView1.Rows.Count - 1)
+
+                    List<string> time = new List<string>();
+                    for (int ii = 0; ii < s[i].Rows.Count - 1; ii++)
                     {
-                        chart1.Series["测点温度"].Points.AddXY(time[nowRow], data[nowRow]);
-                        nowRow++;
+                        time.Add(s[i].Rows[ii].Cells["检测时间"].Value.ToString());
                     }
+
+
+                    List<double> data = new List<double>();
+                    for (int ii = 0; ii < s[i].Rows.Count - 1; ii++)
+                    {
+                        data.Add(Convert.ToDouble(s[i].Rows[ii].Cells[s[i].Columns[0].Name.ToString()].Value));
+                        allData.Add(Convert.ToDouble(s[i].Rows[ii].Cells[s[i].Columns[0].Name.ToString()].Value));
+                    }
+
+
+
+                    int nowRow = 0;
+                    if (s[i].Rows.Count > 1)
+                    {
+                        while (nowRow < s[i].Rows.Count - 1)
+                        {
+                            chart1.Series[s[i].Columns[0].Name.ToString()].Points.AddXY(time[nowRow], data[nowRow]);
+                            nowRow++;
+                        }
+                    }
+
+
                 }
 
-                chart1.ChartAreas[0].AxisY.Minimum = data.Min() * 0.90;
-                chart1.ChartAreas[0].AxisY.Maximum = data.Max() * 1.10;
+                chart1.ChartAreas[0].AxisY.Minimum = allData.Min() * 0.90;
+                chart1.ChartAreas[0].AxisY.Maximum = allData.Max() * 1.10;
 
                 chart1.ChartAreas[0].Axes[1].LabelStyle.Format = "N0";
+
+                ////生成一条曲线
+                //this.chart1.Series.Add("测点温度");
+
+                //chart1.Series["测点温度"].MarkerColor = Color.Black;//设置标志
+                //chart1.Series["测点温度"].MarkerSize = 3;
+                //chart1.Series["测点温度"].MarkerStyle = MarkerStyle.Square;
+                //chart1.Series["测点温度"].IsValueShownAsLabel = true;//是否显示值
+                //chart1.Series["测点温度"].ChartType = SeriesChartType.Spline;//设置显示样式
+                //chart1.Series["测点温度"].BorderWidth = 1;
+                //chart1.Series["测点温度"].Color = Color.Blue;
+                //chart1.Series["测点温度"].ToolTip = "测点" + checkedListBox1.CheckedItems[0].ToString() + comboBox2.Text + "时数据统计";
+                //chart1.Series["测点温度"].YValueType = ChartValueType.Double;
+
+
+                //List<string> time = new List<string>();
+                //for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+                //{
+                //    time.Add(dataGridView1.Rows[i].Cells["检测时间"].Value.ToString());
+                //}
+
+                //List<double> data = new List<double>();
+                //for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+                //{
+                //    data.Add(Convert.ToDouble(dataGridView1.Rows[i].Cells[checkedListBox1.CheckedItems[0].ToString()].Value));
+                //}
+
+
+
+                //int nowRow = 0;
+                //if (dataGridView1.Rows.Count > 1)
+                //{
+                //    while (nowRow < dataGridView1.Rows.Count - 1)
+                //    {
+                //        chart1.Series["测点温度"].Points.AddXY(time[nowRow], data[nowRow]);
+                //        nowRow++;
+                //    }
+                //}
+
+                //chart1.ChartAreas[0].AxisY.Minimum = data.Min() * 0.90;
+                //chart1.ChartAreas[0].AxisY.Maximum = data.Max() * 1.10;
+
+                //chart1.ChartAreas[0].Axes[1].LabelStyle.Format = "N0";
 
             }
             catch (Exception)
@@ -335,6 +424,35 @@ namespace Interface.Forms.DBForms
         private void label6_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            panel1.Controls[0].SendToBack();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            panel1.Controls[panel1.Controls.Count - 1].BringToFront();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            
+            if (true)
+            {
+               
+            }
+
+        }
+
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            for (int i = 0; i < chart1.Series.Count; i++)
+            {
+                this.chart1.Series[i].IsValueShownAsLabel = checkBox1.Checked;
+            }
         }
 
 
